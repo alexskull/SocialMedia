a744aacb87ca52ab98007ce4d1a17008b3bf2339
alexskull <domingoskull3@gmail.com>
2015-10-29 00:59:52 -0430

    Settings Fixed

--- eventosUsuarios/resources/constants/settings.php
+++ eventosUsuarios/resources/constants/settings.php
@@ -3,8 +3,8 @@ class Settings {
     const WEB_MAIN_HOST_URL = "http://localhost/SocialMedia/Eventos/";  
     const WEB_HOST_URL = "http://localhost/SocialMedia/EventosUsuarios/";  
     const WEB_SERVICES_URL = "";  
-    const HOST_URL = "localhost/SocialMedia/EventosUsuarios"; 
-    const URL_SLASHES = 5;
+    const HOST_URL = "localhost/SocialMedia/EventosUsuarios/"; 
+    const URL_SLASHES = 6;
     const SERVER_TIMEZONE = "America/Caracas";
     const SESSION_TIMEOUT = 1800;
     const MAIN_ACTION = "main";
