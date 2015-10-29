98399f6b8df9cdea08332f0b507702d309f9c3db
alexskull <domingoskull3@gmail.com>
2015-10-29 00:27:01 -0430

    Bla

--- Eventos/resources/constants/settings.php
+++ Eventos/resources/constants/settings.php
@@ -3,7 +3,7 @@ class Settings {
     const WEB_HOST_URL = "http://localhost/SocialMedia/Eventos";  
     const WEB_SERVICES_URL = "";  
     const HOST_URL = "localhost/SocialMedia/Eventos"; 
-    const URL_SLASHES = 5;
+    const URL_SLASHES = 6;
     const SERVER_TIMEZONE = "America/Caracas";
     const SESSION_TIMEOUT = 1800;
     const MAIN_ACTION = "main";
